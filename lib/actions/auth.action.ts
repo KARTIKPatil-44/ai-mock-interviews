54126070dc32f57bf12a07d40e7caed6effcc02e
KARTIK-44 <kartikpatilcs@gmail.com>
2025-11-05 10:24:22 +0530

    implement ai agent

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -1,19 +1,18 @@
 'use server';
 
-import { auth, db } from "@/firebase/admin";
-import { CollectionReference, DocumentData, DocumentReference } from "firebase-admin/firestore";
-import { cookies } from "next/headers";
+import {db, auth} from "@/firebase/admin";
+import {cookies} from "next/headers";
 
 const ONE_WEEK = 60 * 60 * 24 * 7;
 
-export async function signUp(params:SignUpParams){
-    const {uid, name, email} = params;
-    
-    try{
+export async function signUp(params: SignUpParams) {
+    const { uid, name, email } = params;
+
+    try {
         const userRecord = await db.collection('users').doc(uid).get();
 
-        if(userRecord.exists){
-            return{
+        if(userRecord.exists) {
+            return {
                 success: false,
                 message: 'User already exists. Please sign in instead.'
             }
@@ -22,57 +21,54 @@ export async function signUp(params:SignUpParams){
         await db.collection('users').doc(uid).set({
             name, email
         })
-        
-        return{
+
+        return {
             success: true,
             message: 'Account created successfully. Please sign in.'
         }
-        
-    }catch(e:any) {
+    } catch (e: any) {
         console.error('Error creating a user', e);
 
-        if(e.code ==='auth/email-already-exists'){
-            return{
+        if(e.code === 'auth/email-already-exists') {
+            return {
                 success: false,
-                message:'This email is already in use '
+                message: 'This email is already in use.'
             }
         }
-        return{
+
+        return {
             success: false,
             message: 'Failed to create an account'
         }
     }
 }
 
-export async function signIn(params:SignInParams) {
-    const {email, idToken} = params;
+export async function signIn(params: SignInParams) {
+    const { email, idToken } = params;
 
-    try{
+    try {
         const userRecord = await auth.getUserByEmail(email);
 
-        if(!userRecord){
-            return{
+        if(!userRecord) {
+            return {
                 success: false,
-                message: 'User dose not exist. Create an account instead.'
+                message: 'User does not exist. Create an account instead.'
             }
         }
 
         await setSessionCookie(idToken);
-
-    }catch(e){
+    } catch (e) {
         console.log(e);
+
         return {
             success: false,
-            message: 'Faild to log into an account.'
+            message: 'Failed to log into an account.'
         }
     }
-    
 }
 
-
-export async function setSessionCookie(idToken:string) {
-
-    const cookieStore =await cookies();
+export async function setSessionCookie(idToken: string) {
+    const cookieStore = await cookies();
 
     const sessionCookie = await auth.createSessionCookie(idToken, {
         expiresIn: ONE_WEEK * 1000,
@@ -85,36 +81,31 @@ export async function setSessionCookie(idToken:string) {
         path: '/',
         sameSite: 'lax'
     })
-
-
-    
 }
 
 export async function getCurrentUser(): Promise<User | null> {
-
     const cookieStore = await cookies();
 
     const sessionCookie = cookieStore.get('session')?.value;
 
     if(!sessionCookie) return null;
 
-    try{
+    try {
         const decodedClaims = await auth.verifySessionCookie(sessionCookie, true);
-        
+
         const userRecord = await db.
             collection('users')
             .doc(decodedClaims.uid)
             .get();
-            
+
         if(!userRecord.exists) return null;
 
-        return{
+        return {
             ...userRecord.data(),
-            id: userRecord.id,  
+            id: userRecord.id,
         } as User;
-
-    }catch(e) {
-        console.log(e);
+    } catch (e) {
+        console.log(e)
 
         return null;
     }
@@ -124,6 +115,4 @@ export async function isAuthenticated() {
     const user = await getCurrentUser();
 
     return !!user;
-    
 }
-
